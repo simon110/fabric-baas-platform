f6016245d885142436487866478bcacaa3822759
Yao-0x1e <Yao.0x1e@outlook.com>
2022-04-21 20:03:24 +0800

    实现JwtUserDetailsService

--- fabric-baas-ttp/src/main/java/com/anhui/fabricbaasttp/service/impl/JwtUserDetailsServiceImpl.java
+++ fabric-baas-ttp/src/main/java/com/anhui/fabricbaasttp/service/impl/JwtUserDetailsServiceImpl.java
@@ -1,14 +1,33 @@
 package com.anhui.fabricbaasttp.service.impl;
 
+import com.anhui.fabricbaasttp.entity.UserEntity;
+import com.anhui.fabricbaasttp.repository.UserRepo;
 import com.anhui.fabricbaasweb.service.JwtUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class JwtUserDetailsServiceImpl implements JwtUserDetailsService {
+    @Autowired
+    private UserRepo userRepo;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<UserEntity> userOptional = userRepo.findById(username);
+        if (userOptional.isPresent()) {
+            UserEntity user = userOptional.get();
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            user.getAuthorities().forEach(authorityName -> authorities.add(new SimpleGrantedAuthority(authorityName)));
+            return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+        }
         return null;
     }
 }
