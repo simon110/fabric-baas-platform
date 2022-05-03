d3a5f9d0e29fd090ea4f97533b5d5f890e6d5833
Yao-0x1e <Yao.0x1e@outlook.com>
2022-05-03 15:41:03 +0800

    删除组织端加入网络申请类中的Orderer定义

--- fabric-baas-org/src/main/java/com/anhui/fabricbaasorg/bean/Participation.java
+++ fabric-baas-org/src/main/java/com/anhui/fabricbaasorg/bean/Participation.java
@@ -4,16 +4,11 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 @ApiModel(value = "加入网络申请信息")
 public class Participation {
     @ApiModelProperty(value = "网络名称")
     private String networkName;
-
-    @ApiModelProperty(value = "预计提供Orderer节点地址", required = true)
-    private List<String> orderers;
 
     @ApiModelProperty(value = "申请处理状态", required = true)
     private int status;
